d763c1b18fb758aef85136d4647f07ea8482608d
Benjamin Carpenter <bcarpenter480@gmail.com>
2025-11-09 22:39:10 +0000

    Initial commit of a Javascript Directed percolation sim!

---
+++ directedPercolation.js
@@ -0,0 +1,111 @@
+let img; // This will hold our off-screen image buffer
+let y = 0; // This will track the current row
+let width;
+let height
+const prob_start = 0.9;
+const prob_survive = 0.7;
+const prob_spread = 0.61;
+let newRow = [];
+let alive = 255;
+let dead = 0;
+let page_size_factor = 5; // How many times the height of the window should the canvas be
+
+function windowResized() {
+    height = windowHeight*page_size_factor;
+    width = windowWidth;
+    resizeCanvas(windowWidth, windowHeight*page_size_factor);
+    setup();
+}
+
+
+
+function setup() {
+    height = windowHeight*page_size_factor;
+    width = windowWidth;
+    createCanvas(windowWidth, windowHeight*page_size_factor);
+    console.log("Canvas size: " + width + "x" + height);
+    // 1. Create a blank p5.Image
+    img = createImage(width, height*page_size_factor);
+
+    // 2. Load its pixel array ONCE
+    // This is the buffer we will write our data into
+    img.loadPixels();
+
+    // 3. Initialize the first row
+    newRow = gen_first_row();
+
+}
+
+
+function get_next_row(currentRow) {
+    let _next_row = [];
+    for (let x = 0; x < currentRow.length; x++) {
+        if (currentRow[x] == alive) {
+            // Cell is alive, it stays alive if random < prob_survive
+            if (random(1) < prob_survive) {
+                _next_row.push(alive);
+                // If cell survives, it can spread to neighbors
+                if (x > 0) {
+                    _next_row[x - 1] = random(1) < prob_spread ? alive : _next_row[x - 1];
+                }
+                if (x < currentRow.length - 1) {
+                    _next_row[x + 1] = random(1) < prob_spread ? alive : _next_row[x + 1];
+                    }
+            }
+
+        } else {
+            _next_row.push(dead);
+            }
+
+
+    }
+    return _next_row;
+}
+
+function gen_first_row() {
+    let firstRow = [];
+    for (let x = 0; x < width; x++) {
+        if (random(1) < prob_start) {
+            firstRow.push(255); // alive
+        } else {
+            firstRow.push(0); // dead
+        }
+    }
+    return firstRow;
+}
+
+function draw() {
+  // Check if we are done
+  if (y >= height*page_size_factor) {
+    noLoop();
+    console.log("Generation complete.");
+    return;
+  }
+
+  newRow = get_next_row(newRow);
+
+
+  // 2. Write this new row into the img.pixels[] array
+  for (let x = 0; x < newRow.length; x++) {
+    let val = newRow[x];
+
+    // Find the index in the 1D pixel array
+    let index = (y % height * width + x) * 4;
+
+    // Set R, G, B, and Alpha channels for the off-screen image
+    img.pixels[index + 0] = val;
+    img.pixels[index + 1] = val;
+    img.pixels[index + 2] = val;
+    img.pixels[index + 3] = 255;
+  }
+
+  // 3. Apply all pixel changes to the off-screen image
+  img.updatePixels();
+
+  // 4. Draw the entire updated image to the canvas at once
+  // This is a single, fast operation.
+  image(img, 0, 0);
+
+  // 5. Move to the next row for the next frame
+  y++;
+}
